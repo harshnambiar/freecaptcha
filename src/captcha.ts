1f1f5af939ecbe2b469225185e431a0f65316c44
harshnambiar <harrisonlvl2@gmail.com>
2024-03-08 15:05:27 +0530

    added deception text

--- src/captcha.ts
+++ src/captcha.ts
@@ -1,5 +1,7 @@
 import { Captcha } from './types'; 
 
+
+// prompt the use to only enter White -> Blue -> Red text in that order
 export function createCaptcha(canvas: HTMLCanvasElement): string {
     const ctx=canvas.getContext('2d')!;
     ctx.fillStyle= 'black';// make screen black
@@ -31,6 +33,17 @@ function addTextToImage(imagePath: string, text: string, circle_canvas: HTMLCanv
     img.width = 600;
     var shift1 = 55 + Math.floor(Math.random() * 5);
     var shift2 = 35 + shift1 - Math.floor(Math.random() * 5);
+
+    var charsArray =
+    "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var lengthRnd = 6;
+    var rnd = "";
+    for (var i = 0; i < lengthRnd; i++) {
+        var index = Math.floor(Math.random() * charsArray.length); //get the next character from the array
+        rnd = rnd.concat(charsArray[index]);
+    }
+    var rtext1 = rnd.substring(0, 3);
+    var rtext2 = rnd.substring(3, 6);
     if (order == 0) {
         var text1 = text.substring(0, 3);
         var text2 = text.substring(3, 6);
@@ -47,6 +60,12 @@ function addTextToImage(imagePath: string, text: string, circle_canvas: HTMLCanv
             context.fillStyle = "#FF0000";
             context.font = "25px bold courier";
             context.fillText(text3, 140, shift2);
+            context.fillStyle = "#00FF00";
+            context.font = "25px bold courier";
+            context.fillText(rtext1, 10, shift2);
+            context.fillStyle = "#FFFF00";
+            context.font = "25px bold courier";
+            context.fillText(rtext2, 140, 40);
         };
     }
 
@@ -67,6 +86,12 @@ function addTextToImage(imagePath: string, text: string, circle_canvas: HTMLCanv
             context.fillStyle = "indigo";
             context.font = "25px bold courier";
             context.fillText(text2, 140, shift2);
+            context.fillStyle = "#00FF00";
+            context.font = "25px bold courier";
+            context.fillText(rtext1, 10, shift2);
+            context.fillStyle = "#FFFF00";
+            context.font = "25px bold courier";
+            context.fillText(rtext2, 140, 40);
         };
 
     }
@@ -86,6 +111,12 @@ function addTextToImage(imagePath: string, text: string, circle_canvas: HTMLCanv
             context.fillStyle = "indigo";
             context.font = "25px bold courier";
             context.fillText(text2, 140, shift2);
+            context.fillStyle = "#00FF00";
+            context.font = "25px bold courier";
+            context.fillText(rtext1, 10, shift2);
+            context.fillStyle = "#FFFF00";
+            context.font = "25px bold courier";
+            context.fillText(rtext2, 140, 40);
         };
 
     }
@@ -105,6 +136,12 @@ function addTextToImage(imagePath: string, text: string, circle_canvas: HTMLCanv
             context.fillStyle = "#FF0000";
             context.font = "25px bold courier";
             context.fillText(text3, 140, shift2);
+            context.fillStyle = "#00FF00";
+            context.font = "25px bold courier";
+            context.fillText(rtext1, 10, shift2);
+            context.fillStyle = "#FFFF00";
+            context.font = "25px bold courier";
+            context.fillText(rtext2, 140, 40);
         };
 
     }
@@ -124,6 +161,12 @@ function addTextToImage(imagePath: string, text: string, circle_canvas: HTMLCanv
             context.fillStyle = "#FFFFFF";
             context.font = "25px bold courier";
             context.fillText(text1, 140, shift2);
+            context.fillStyle = "#00FF00";
+            context.font = "25px bold courier";
+            context.fillText(rtext1, 10, shift2);
+            context.fillStyle = "#FFFF00";
+            context.font = "25px bold courier";
+            context.fillText(rtext2, 140, 40);
         };
 
     }
@@ -143,6 +186,12 @@ function addTextToImage(imagePath: string, text: string, circle_canvas: HTMLCanv
             context.fillStyle = "#FFFFFF";
             context.font = "25px bold courier";
             context.fillText(text1, 140, shift2);
+            context.fillStyle = "#00FF00";
+            context.font = "25px bold courier";
+            context.fillText(rtext1, 10, shift2);
+            context.fillStyle = "#FFFF00";
+            context.font = "25px bold courier";
+            context.fillText(rtext2, 140, 40);
         };
 
     }
